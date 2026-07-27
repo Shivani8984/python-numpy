8a5120892a6ee6da8ddbdda96373bf6df71bf78c
Shivani Patel <shivani8984@gmail.com>
2026-07-27 14:17:55 -0400

    more examples from matplotlib

--- python-pandas/pandas-matplotlib.py
+++ python-pandas/pandas-matplotlib.py
@@ -25,6 +25,8 @@ df = pd.read_csv('./data/transaction1.csv')
 
 dates  = df["Date"]
 sales = df["SalesAmount"]
+# plt.hist(sales)
+# plt.plot(dates,sales, 'o')
 
 # plt.plot(
 #     dates,
@@ -40,7 +42,31 @@ sales = df["SalesAmount"]
 # plt.show()
 
 # Create the objects
-fig, ax = plt.subplots()
-fig.suptitle("2026 Sales")
-ax.plot(dates, sales)
+# fig, ax = plt.subplots()
+# fig.suptitle("2026 Sales")
+# ax.plot(dates, sales)
+# plt.show()
+
+plt.title("Sales Data", fontweight="bold", fontfamily="monospace", fontsize=28)
+
+plt.xlabel("Date", color="green", fontsize=14, fontweight='bold')
+
+plt.ylabel('Amount', color="green", fontsize=14, fontweight='bold')
+
+plt.grid()
+
+# create a new subplot
+plt2 = plt.subplot(1,2,2)
+# plot the new data
+plt2.plot(df1["Date"], df1["SalesAmount"])
+# plt.tight_layout(pad=2.5)
+
+# create the figure and axes objects
+# fig, ax = plt.subplots()
+
+# fig.suptitle('2026 Sales')
+
+# ax.set_title('Axes', loc='left', fontstyle='oblique', fontsize='medium')
+# ax.plot(dates, sales)
+
 plt.show()
